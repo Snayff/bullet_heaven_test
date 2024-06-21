229d2c7475df590f02adeba5f2d00f176d578592
Snayff <snaith2@gmail.com>
2024-06-21 19:47:02 +0100

    finished gd quest vs tutorial

---
+++ pistol/gun.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+func _physics_process(delta: float) -> void:
+	var enemies_in_range: Array[Node2D] = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target_enemy: Node2D = enemies_in_range[0]
+		look_at(target_enemy.global_position)
+
+func shoot() -> void:
+	const BULLET: PackedScene = preload("res://pistol/bullet.tscn")
+	var new_bullet: Node = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout() -> void:
+	shoot()
